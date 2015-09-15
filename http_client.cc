323e0dba0b0cff33cfad82d692c740f9637eaeb9
cullenstr <cullens22@gmail.com>
2015-09-15 18:17:09 -0400

    added Lauren's client code

--- http_client.cc
+++ http_client.cc
@@ -9,7 +9,10 @@
 #include <sys/types.h>          /* See NOTES */
 #include <sys/socket.h>
 #include <netdb.h>
-
+#include <unistd.h> //needed to access close function under gcc 4.7+
+#include <iostream> //includes needed to manipulate and create strings
+#include <string>
+using namespace std;
 
 #define BUFSIZE 1024
 
@@ -19,7 +22,9 @@ int main(int argc, char * argv[]) {
     int server_port    = -1;
     char * server_path = NULL;
     char * req         = NULL;
+	char buff_in[BUFSIZE];
     bool ok            = false;
+	int return_val     = 0;
 
     /*parse args */
     if (argc != 5) {
@@ -49,35 +54,94 @@ int main(int argc, char * argv[]) {
     }
 
     /* make socket */
-
+	int client_socket = socket(AF_INET, SOCK_STREAM,0);
     /* get host IP address  */
     /* Hint: use gethostbyname() */
-
+	struct addrinfo hints;
+	struct addrinfo *return_addresses;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	char * port_as_string;
+	sprintf( port_as_string, "%d", server_port);
+	return_val = getaddrinfo(server_name, port_as_string, &hints, &return_addresses);
+	if (return_val != 0) {
+		fprintf(stderr, "getaddrinfo() failed: %s\n", gai_strerror(return_val));
+		exit(-1);
+	}
     /* set address */
-
+	struct sockaddr *sa;
+	sa = return_addresses->ai_addr;
     /* connect to the server socket */
-
+	return_val = connect(client_socket, sa, sizeof(sa));
+	if (return_val != 0) {
+		perror("Error on connection :");
+		exit(-1);
+	}
     /* send request message */
     sprintf(req, "GET %s HTTP/1.0\r\n\r\n", server_path);
-
+	send(client_socket, req, strlen(req) + 1, 0);
     /* wait till socket can be read. */
     /* Hint: use select(), and ignore timeout for now. */
-
+	fd_set server_fd_set;
+	fd_set temp_fd_set;
+	FD_ZERO(&server_fd_set);
+	FD_ZERO(&temp_fd_set);
+	FD_SET(client_socket, &server_fd_set);
+	temp_fd_set = server_fd_set;
+	return_val = select( client_socket+1, &temp_fd_set, NULL, NULL, NULL);
+	if( return_val == -1) {
+		perror("Error waiting for server");
+		exit(-1);
+	}
     /* first read loop -- read headers */
-
+	memset(buff_in, 0, sizeof(buff_in));
+	return_val = recv(client_socket, buff_in, sizeof(buff_in), 0); //if return_val is 0, we've got all the data
+	//but we'll assume all the headers fit in the first buffer
+	if (return_val < 0) {
+		perror("Error reading from socket");
+		exit(-1);
+	}
+	bool more_to_read = true;
+	if (return_val == 0) {
+		more_to_read = false;
+	}
     /* examine return code */   
-
+	string http_content;
+	string return_code;
+	http_content.assign(buff_in); //proper way to assign char* to string in c++?
     //Skip "HTTP/1.0"
-    //remove the '\0'
-
+    //remove the '\0' //not sure what he means by this?? check in telnet
+	return_code = http_content.substr(9, 3); //extract return code
+	string all_ok = "200";
     // Normal reply has return code 200
-
+	if (return_code.compare(all_ok) != 0) {
+		ok = false;
+	}
     /* print first part of response: header, error code, etc. */
-
+	int substring_marker;
+	substring_marker = http_content.find("<http>"); //might need to be inside non-error loop?
+	fprintf(stdout, "%s", (http_content.substr(0, substring_marker)).c_str());
+	//cout << http_content.substr(0, substring_marker) << endl; would need to include iostream
     /* second read loop -- print out the rest of the response: real web content */
-
+	if (ok) { //if there is html to read
+		fprintf(stdout, "%s", (http_content.substr(substring_marker)).c_str());
+		while (more_to_read != false) {
+			memset(buff_in, 0, sizeof(buff_in));
+			return_val = recv(client_socket, buff_in, sizeof(buff_in), 0); 
+			if (return_val < 0) {
+				perror("Error reading from socket");
+				exit(-1);
+			}
+			if (return_val == 0) {
+				more_to_read = false;
+			}
+			fprintf(stdout, buff_in);
+		}	
+	}
     /*close socket and deinitialize */
-
+	close(client_socket);
+	freeaddrinfo(return_addresses);
     if (ok) {
 	return 0;
     } else {

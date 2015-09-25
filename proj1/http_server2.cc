30c78f0a8f8f1234f434e98baf6890501f6496a5
cullenstr <cullens22@gmail.com>
2015-09-25 15:57:34 -0400

    server2 should be functional now; it accepts multiple connections at a time

--- proj1/http_server2.cc
+++ proj1/http_server2.cc
@@ -10,23 +10,28 @@
 #include <sys/types.h>          /* See NOTES */
 #include <sys/socket.h>
 #include <netdb.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
-
+#include <iostream>
 
 #define BUFSIZE 1024
 #define FILENAMESIZE 100
+using namespace std;
 
 int handle_connection(int sock);
 
-int main(int argc, char * argv[]) {
+int main(int argc, char * argv[]) 
+{
     int server_port = -1;
     int rc          =  0;
-    int accept_sock        = -1;
-	//struct sockaddr_storage remoteaddr;
-	//socklen_t addr_size;
-
+    int accept_sock = -1;
+	int sock_a      = -1;
+    int fdmax       = -1;
+    int connections;
+    struct sockaddr_in sa;
+    struct timeval t;
+    t.tv_usec = 0; t.tv_sec = 5; // 5 sec timeout
+	
     /* parse command line args */
     if (argc != 3) {
 	fprintf(stderr, "usage: http_server1 k|u port\n");
@@ -60,12 +65,14 @@ int main(int argc, char * argv[]) {
 		perror("Error on socket(): ");
 		exit(-1);
 	}
+	fdmax = accept_sock;
+	
     /* set server address*/
-	struct sockaddr_in sa;
 	memset(&sa, 0, sizeof(sa));
 	sa.sin_port = htons(server_port);
 	sa.sin_addr.s_addr = htonl(INADDR_ANY);
 	sa.sin_family = AF_INET;
+	
     /* bind listening socket */
 	rc = bind(accept_sock, (struct sockaddr *)&sa,sizeof(sa));
 	if (rc==-1) {
@@ -78,68 +85,79 @@ int main(int argc, char * argv[]) {
 		perror("Error on listen(): ");
 		exit(-1);
 	}
+		
     /* connection handling loop: wait to accept connection */
+	cout<<"Server status: Waiting...\n";
+	
 	//initialize list of connections
 	fd_set connection_list;
 	FD_ZERO(&connection_list);
 	FD_SET(accept_sock, &connection_list); //remove for minet
-	int fdmax = accept_sock; //set to 0 in case of minet
+	
 	/*minet
 	fd_set minet_connection_list;
 	FD_ZERO(&minet_connection_list);
 	FD_SET(accept_sock, &minet_connection_list);
 	int minet_fdmax = accept_sock;
 	*/
-    while (1) {
+	
+	/* create read list */
+	fd_set read_list;
+	FD_ZERO(&read_list);
+	
+	/*minet:
+	fd_set minet_read_list;
+	FD_ZERO(&minet_read_list);
+	FD_SET(connect_sock, &minet_read_lsit);
+	minet_read_list = minet_connection_list;
+	*/
+		
+    while (1) 
+	{
 		/* create read list */
-		fd_set read_list;
-		FD_ZERO(&read_list);
-		read_list = connection_list;
-		/*minet:
-		fd_set minet_read_list;
-		FD_ZERO(&minet_read_list);
-		FD_SET(connect_sock, &minet_read_lsit);
-		minet_read_list = minet_connection_list;
-		*/
+		memcpy(&read_list, &connection_list, sizeof(&connection_list));
+		
 		/* do a select */
-		rc = select( fdmax+1, &read_list, NULL, NULL, NULL); //remove in case of minet
+		connections = select( fdmax+1, &read_list, NULL, NULL, &t); //remove in case of minet
 		//return_val = minet_select( minet_fdmax+1, &minet_read_list, NULL, NULL, fdmax+1, &read_list, NULL, NULL, NULL);
-		if( rc == -1) {
-			perror("Error waiting for server");
-			exit(-1);
-		}
+		if (connections < 0) 
+		{ 
+			perror("Select failed.\n"); exit(-1);
+		}	
+		
 		/* process sockets that are ready */
-		for( int i = 0; i <= fdmax; ++i ) {
-			if( FD_ISSET( i, &read_list) ) {
-				if(i == accept_sock) {
-					/* for the accept socket, add accepted connection to connections */
-					struct sockaddr_in sa_connect;
-					int connect_sock;
+		for(int i=0; (i <= fdmax) && (connections>0); ++i) 
+		{
+			if(FD_ISSET(i,&read_list)) 
+			{
+				connections--;
+				/* for the accept socket, add accepted connection to connections */
+				if(i == accept_sock) 
+				{
 					int size=sizeof(struct sockaddr_in); 
-					if((connect_sock = accept(accept_sock, (struct sockaddr *)&sa_connect, (socklen_t*)&size))==-1)
-						printf("error in accept(): %s\n", strerror(errno));
-					else {
-						FD_SET(connect_sock, &connection_list);
-						if( connect_sock > fdmax ) {
-							fdmax = connect_sock;
-						}
-						rc = handle_connection(connect_sock);
+					if ((sock_a = accept(i,(struct sockaddr *)&sa, (socklen_t*)&size)) <0) 
+					{
+						perror("Accept failed.\n"); exit (-1);
 					}
-				}
-				else {
+					cout<<"Added an accepted connection to connections.\n";
+
 					/* for a connection socket, handle the connection */
-					if (i == fdmax) {
-						//we need to find the new max
-						int new_max = 0;
-						//have to iterate through connection list to avoid using closed fd that were left in read list
-						for(int j = 0; j < fdmax; ++j){
-							if( FD_ISSET(j, &connection_list) && j!=i && j>new_max)
-								new_max = j;
-						}
-						fdmax = new_max;
+					FD_SET(sock_a, &connection_list);
+					if (sock_a > fdmax)
+						fdmax = sock_a;
+				}
+				else 
+				{
+					rc = handle_connection(i);
+					if (rc < 0) 
+						perror("handle_connection error.\n"); 
+					close(i);
+					FD_CLR(i, &connection_list); //remove i from set_socks
+					cout<<"Removed a connection from the list of connections.\n";
+					if (i == fdmax) 
+					{
+						while ( !(FD_ISSET(fdmax, &connection_list))) fdmax--; 
 					}
-					FD_CLR(i, &connection_list); //remove from list of connections
-					rc = handle_connection(i); //handle connection, close socket
 				}
 			}
 		}

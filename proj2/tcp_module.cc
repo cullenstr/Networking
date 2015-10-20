35874f58fec7d00263bf4ec0ee4b8101534ec71c
Lauren T <let41@pitt.edu>
2015-10-19 21:13:59 -0400

    Step 12 partial, basic packet in/out

--- proj2/tcp_module.cc
+++ proj2/tcp_module.cc
@@ -22,16 +22,17 @@
 #include <iostream>
 
 #include "Minet.h"
+#include "tcpstate.h" //pretty sure we have to rewrite TCPState, but better check first.  The provided one should work for now
 
 using namespace std;
 
-struct TCPState {
+/*struct TCPState {
     // need to write this
     std::ostream & Print(std::ostream &os) const { 
 	os << "TCPState()" ; 
 	return os;
     }
-};
+};*/
 
 
 int main(int argc, char * argv[]) {
@@ -72,7 +73,9 @@ int main(int argc, char * argv[]) {
 
     MinetEvent event;
     double timeout = 1;
+	
 
+	
     while (MinetGetNextEvent(event, timeout) == 0) {
 
 	if ((event.eventtype == MinetEvent::Dataflow) && 
@@ -80,6 +83,51 @@ int main(int argc, char * argv[]) {
 	
 	    if (event.handle == mux) {
 			MinetSendToMonitor(MinetMonitoringEvent("Packet has arrived..."));
+			//step 12 prereq: have a hardcoded connection
+			Connection c;
+			TCPState listen_state(1, LISTEN, 1);
+			ConnectionToStateMapping<TCPState> c_mapping (c, timeout, listen_state, false ); //need to set timeout.  What should initial value be?  Check rfc
+			//we're assuming the current state is LISTEN
+			Packet p;
+			Packet send_p;
+			IPHeader iph;
+			iph=p.FindHeader(Headers::IPHeader);
+			TCPHeader tcph;
+			tcph=p.FindHeader(Headers::TCPHeader);
+			unsigned char flags=0;
+			tcph.GetFlags(flags);
+			if(IS_SYN(flags)==True) {
+				//this is our syn request
+				iph.GetDestIP(c.src); //fill out connection
+				iph.GetSourceIP(c.dest);
+				iph.GetProtocol(c.protocol);
+				tcph.GetDestPort(c.srcport);
+				tcph.GetSourcePort(c.destport);
+				c.state.SetState(SYN_RCVD);
+				clist.push_back(c); //add connection to connection list
+				//ConnectionToStateMapping<TCPState> c_mapping(c, timeout, c_state, false); 
+				//connection saved.  Now we send an syn_ack
+				//lets reuse the iph and tcph headers
+				iph.SetSourceIP(c.connection.src);
+				iph.SetDestIP(c.connection.dest);
+				iph.SetTotalLength(IP_HEADER_BASE_LENGTH + TCP_HEADER_BASE_LENGTH); //there is no data in an syn_ack
+				iph.SetProtocol(IP_PROTO_TCP);
+				send_p.PushFrontHeader(iph); //I think IP should be the front header?  
+				tcph.SetSourcePort(c.connection.src, send_p);
+				tcph.SetDestPort(c.connection.dest, send_p);
+				tcph.SetHeaderLen(TCP_HEADER_BASE_LENGTH, send_p);
+				flags = 0;
+				SET_ACK(flags);
+				tcph.SetFlags(flags, send_p);
+				//tcph.SetAckNum not sure how to set these yet?
+				//tcph.SetSeqNum
+				//tcph.SetWinSize
+				send_p.PushBackHeader(tcph); //if IP is front, tcp is back
+				//okay, now we have two headers 
+				MinetSend(mux, send_p);
+			}
+			//not syn, could be ack after syn.  Let's check the list!
+			//ConnectionList<UDPState>::iterator cs = clist.FindMatching(c);
 		// ip packet has arrived!
 	    }
 
